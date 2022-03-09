ee279399985fcac38a2417caed78364588e83480
jyotsna-penumaka <jpenumak@redhat.com>
2022-03-09 09:22:04 -0500

    Add return structs and tests for create_log_entry, get_log_entry_by_index, get_log_entry_by_uuid
    Signed-off-by: jyotsna-penumaka <jpenumak@redhat.com>

--- test_rekor/src/main.rs
+++ test_rekor/src/main.rs
@@ -1,5 +1,5 @@
 use openapi::apis::{configuration::Configuration, entries_api};
-use openapi::models::{ProposedEntry, log_entry::LogEntry};
+use openapi::models::{ProposedEntry, log_entry::LogEntry, SearchLogQuery};
 use openapi::models::rekord::{Hash, Data, PublicKey, Signature, Spec};
 use url::Url;
 
@@ -11,7 +11,7 @@ async fn main() {
     println!("Test#1 create_log_entry!");
     let hash = Hash::new(
         "sha256".to_string(),
-        "0defde9b5c64102ed157d35ca0bdaf3208846484f759c75799f03f55f973fd5b".to_string(),
+        "e2535d638859bb63ea9ea5cf467562cba63b007eae1acd0d73a3f259c582561f".to_string(),
     );
     let data = Data::new(
         hash,
@@ -22,7 +22,7 @@ async fn main() {
     );
     let signature = Signature::new(
         "ssh".to_string(),
-        "LS0tLS1CRUdJTiBTU0ggU0lHTkFUVVJFLS0tLS0KVTFOSVUwbEhBQUFBQVFBQUFETUFBQUFMYzNOb0xXVmtNalUxTVRrQUFBQWcvdmVTYzRvbHBLdE1vT1I3cndmOFZHSHpoaApnMEZJb0R0YzVSMkpsdHpHZ0FBQUFFWm1sc1pRQUFBQUFBQUFBR2MyaGhOVEV5QUFBQVV3QUFBQXR6YzJndFpXUXlOVFV4Ck9RQUFBRUNNaXJBNmNUMVUzRTUrM0pkWURhdTFVWG1ZcWRzMUxPMVppN04vYVh2a2puWW4yYmlFUzBYUTJ4R3FNOGFXQUYKUU4zTWcxYmxPL3B4UzQzcURER3pBUAotLS0tLUVORCBTU0ggU0lHTkFUVVJFLS0tLS0K".to_string(),
+        "LS0tLS1CRUdJTiBTU0ggU0lHTkFUVVJFLS0tLS0KVTFOSVUwbEhBQUFBQVFBQUFETUFBQUFMYzNOb0xXVmtNalUxTVRrQUFBQWcvdmVTYzRvbHBLdE1vT1I3cndmOFZHSHpoaApnMEZJb0R0YzVSMkpsdHpHZ0FBQUFFWm1sc1pRQUFBQUFBQUFBR2MyaGhOVEV5QUFBQVV3QUFBQXR6YzJndFpXUXlOVFV4Ck9RQUFBRURCYVFtVTRXNHFCQzBaM3N6aTZuSEE4cWlBdE5QVzFkU29UTmtlMTBOKzRRTUdad0pRMXR6QTVIYk5BUkxHc3cKN0I0b2RxWWFpRVEwSzMwdEtBZEcwSAotLS0tLUVORCBTU0ggU0lHTkFUVVJFLS0tLS0K".to_string(),
         public_key,
     );
     let spec = Spec::new(signature, data);
@@ -32,21 +32,30 @@ async fn main() {
             api_version: "0.0.1".to_string(),
             spec: spec,
         };
-        
 
-/*     let uuid: &str = &response[1..67];
-    let rest: &str = &response[69..response.len() - 2];
-    let sum = "{\"uuid\": ".to_string() + &(uuid.to_owned()) + "," + rest;
-    let v: Result<LogEntry, serde_json::Error> = serde_json::from_str(&sum);
-    v.map_err(|err| Box::new(err) as Box<dyn std::error::Error>) */
-
-    let message: LogEntry = entries_api::create_log_entry(&configuration, proposed_entry).await.unwrap();
-    println!("{:#?}", message);
+    //let log_entry: LogEntry = entries_api::create_log_entry(&configuration, proposed_entry).await.unwrap();
+    //println!("{:#?}", log_entry);
 
     // Test get_log_entry_by_index 
-/*     println!("Test#2 get_log_entry_by_index");
-    let message = entries_api::get_log_entry_by_index(&configuration, 1).await;
-    println!("{:#?}", message); */
+    println!("Test#2 get_log_entry_by_index");
+    //let message: LogEntry = entries_api::get_log_entry_by_index(&configuration, 1).await.unwrap();
+    //println!("{:#?}", message);
 
+    // Test get_log_entry_by_uuid 
+     println!("Test#3 get_log_entry_by_uuid");
+    // let uuid = "073970a07c978b7a9ff15b69fe15d87dfb58fd5756086e3d1fb671c2d0bd95c0";
+    // let message: LogEntry = entries_api::get_log_entry_by_uuid(&configuration, &uuid).await.unwrap();
+    // println!("{:#?}", message);  
+
+    // Test search_log_query NoneNone
+    println!("Test#3 search_log_query");
+
+    let query = SearchLogQuery {
+        entry_uuids: Some(vec!["3bb6b37275642a289650d81cd5ba8aba0adcf9b5a5330d21bf9dd6157f4cbb67".to_string()]),
+        log_indexes: Some(vec![1594206]),
+        entries: Some(vec![proposed_entry]),
+    };
+    let message = entries_api::search_log_query(&configuration, query).await.unwrap();
+    println!("{:#?}", message);
 
 }
